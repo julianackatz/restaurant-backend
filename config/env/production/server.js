d0797a4bcc7d681267034a8aa477d2edfcf70fec
Juliana Katz <julianalimacunha@hotmail.com>
2023-10-03 21:14:38 -0400

    update server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,7 @@
-
 module.exports = ({ env }) => ({
-    proxy: true,
-    url: env('APP_URL'), // Sets the public URL of the application.
-    app: { 
-      keys: env.array('APP_KEYS')
-    },
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
 });
+
+const strapi = require('@strapi/strapi');
+strapi(/* {...} */).start();
